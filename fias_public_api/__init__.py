cefe8c23b98444788e8f27417bcbd7238e98c661
quonaro <quonaro@mail.ru>
2025-11-10 13:04:10 +0700

    Refactor FIAS Public API module to include AsyncFPA and SyncFPA; update __all__ exports for better module structure

--- fias_public_api/__init__.py
+++ fias_public_api/__init__.py
@@ -1,5 +1,15 @@
 from .constants import STANDART_HEADERS
-from .fias_async import get_token_async, FiasPublicApi
-from .fias_sync import get_token_sync, FiasPublicApi
+from .fias_async import get_token_async, AsyncFPA
+from .fias_sync import get_token_sync, SyncFPA
+from . import constants, fias_async, fias_sync
 
-__all__ = ["STANDART_HEADERS", "get_token_async", "get_token_sync"]
+__all__ = [
+    "STANDART_HEADERS",
+    "get_token_async",
+    "get_token_sync",
+    "AsyncFPA",
+    "SyncFPA",
+    "constants",
+    "fias_async",
+    "fias_sync",
+]
